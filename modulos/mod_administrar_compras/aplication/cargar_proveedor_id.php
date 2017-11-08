1050613b1342987207308d520e028a0c01f54638
jhosagid7 <jhosagid7@gmail.com>
2017-11-08 09:38:42 -0400

    commit

--- modulos/mod_administrar_compras/aplication/cargar_proveedor_id.php
+++ modulos/mod_administrar_compras/aplication/cargar_proveedor_id.php
@@ -1,22 +1,22 @@
 <?php
 //Este archivo me permite cargar los proveedores por medio del ide*/
 if (is_file('../../../connection/conexion.php')) {
-	include_once "../../../connection/conexion.php";
+    include_once "../../../connection/conexion.php";
 }
 //Funcion que me permite llamar el proveedor
 function getProveedorId()
 {
-	$mysqli = getConn();
-	$id = htmlspecialchars($_POST['id_proveedor']);
-	$query = "SELECT * FROM proveedor WHERE id_proveedor = $id";
-	$result = $mysqli->query($query);
-	$reg = mysqli_num_rows($result);
-	if ($reg) {
+    $mysqli = getConn();
+    $id = $_POST['id'];
+    $query = "SELECT * FROM proveedor WHERE id_proveedor = $id";
+    $result = $mysqli->query($query);
+    $reg = mysqli_num_rows($result);
+    if ($reg) {
         /* $list = $list->fetch_array(MYSQLI_ASSOC); */
-		while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-			$data["data"] = $row;
-		}
-		echo json_encode($data);
-	}
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $data["data"] = $row;
+        }
+        echo json_encode($data);
+    }
 }
 echo getProveedorId();
